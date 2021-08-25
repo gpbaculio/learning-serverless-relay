96efc62c481e18096637c8639f95214546ee03f4
gpbaculio <gpbaculio@gmail.com>
2021-08-25 22:24:30 +0800

    add drawer animation and viewer query

---
+++ reddit-top/src/Drawer.tsx
@@ -0,0 +1,60 @@
+import { useEffect, useRef, useState } from "react";
+import { graphql } from "babel-plugin-relay/macro";
+import { useLazyLoadQuery } from "react-relay";
+import { useSpring, animated } from "react-spring";
+import styled from "styled-components";
+import { useSwipeable } from "react-swipeable";
+
+import Listings from "./Listings";
+import { DrawerQuery } from "./__generated__/DrawerQuery.graphql";
+
+const Drawer = () => {
+  const { viewer } = useLazyLoadQuery<DrawerQuery>(DrawerGraphQL, {});
+  const containerRef = useRef<HTMLDivElement>(null);
+  const [containerWidth, setContainerWidth] = useState(0);
+  const [hideDrawer, setHideDrawer] = useState(false);
+
+  const handlers = useSwipeable({
+    onSwiped: () => {
+      setHideDrawer((t) => !t);
+    },
+    onTap: () => {
+      setHideDrawer((t) => !t);
+    },
+    trackMouse: true,
+  });
+
+  useEffect(() => {
+    if (containerRef && containerRef.current) {
+      const { width } = containerRef.current.getBoundingClientRect();
+      setContainerWidth(width);
+    }
+  }, [containerRef]);
+
+  const styles = useSpring({
+    transform: `translateX(${hideDrawer ? -containerWidth / 1.2 : 0}px)`,
+  });
+
+  return (
+    <Container {...{ style: styles, ...handlers, ref: containerRef }}>
+      {viewer && <Listings viewer={viewer} />}
+    </Container>
+  );
+};
+
+export default Drawer;
+
+const DrawerGraphQL = graphql`
+  query DrawerQuery {
+    viewer {
+      user
+      id
+      ...ListingsPagination_viewer
+    }
+  }
+`;
+
+const Container = styled(animated.div)`
+  height: 100%;
+  background-color: red;
+`;
