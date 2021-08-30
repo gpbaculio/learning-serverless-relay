a1a1044e28f252c23b67942e43eee025daf7a120
gpbaculio <gpbaculio@gmail.com>
2021-08-30 14:42:25 +0800

    add theme

---
+++ reddit-top/src/theme.d.ts
@@ -0,0 +1,11 @@
+import "styled-components";
+
+declare module "styled-components" {
+  export interface DefaultTheme {
+    orange: string;
+    white: string;
+    time: string;
+    background: string;
+    dot: string;
+  }
+}
---
+++ reddit-top/src/theme.ts
@@ -0,0 +1,11 @@
+const theme = {
+  orange: "#FF4500",
+  white: "#ffffff",
+  time: "#f5f5f5",
+  background: "#000000",
+  darkGray: "#63666A",
+  dot: "#4285F4",
+  circle: "#187cf7",
+};
+
+export default theme;
