477d352719fedee4de616cae9939dab68165df9a
gpbaculio <gpbaculio@gmail.com>
2021-08-31 17:12:45 +0800

    create function to set initial values of node

--- reddit-top/src/Drawer/Listings/Listings.tsx
+++ reddit-top/src/Drawer/Listings/Listings.tsx
@@ -10,8 +10,8 @@ import Listing from "./Listing";
 
 import { ListingsPaginationQuery } from "../../__generated__/ListingsPaginationQuery.graphql";
 import { ListingsPagination_viewer$key } from "../../__generated__/ListingsPagination_viewer.graphql";
-import { RecordSourceSelectorProxy, StoreUpdater } from "relay-runtime";
 import { ListingFragmentGraphQL_listing$data } from "../../__generated__/ListingFragmentGraphQL_listing.graphql";
+import { useCallback } from "react";
 
 interface ListingsProps {
   viewer: ListingsPagination_viewer$key;
@@ -24,6 +24,19 @@ const Listings = ({ viewer }: ListingsProps) => {
     ListingsPagination_viewer$key
   >(ListingsGraphQL, viewer);
 
+  const setNodeLocalDefaultValues = useCallback(
+    (id: string) => {
+      commitLocalUpdate(environment, (store) => {
+        const list = store.get<ListingFragmentGraphQL_listing$data>(id);
+        if (list) {
+          list.setValue(false, "isDismissed");
+          list.setValue(false, "isRead");
+        }
+      });
+    },
+    [environment]
+  );
+
   return (
     <>
       <StyledUl>
@@ -31,17 +44,7 @@ const Listings = ({ viewer }: ListingsProps) => {
           data.listings.edges &&
           data.listings.edges.map((edge, i) => {
             if (edge && edge.node) {
-              commitLocalUpdate(environment, (store) => {
-                if (edge.node && edge.node.id) {
-                  const list = store.get<ListingFragmentGraphQL_listing$data>(
-                    edge.node.id
-                  );
-                  if (list) {
-                    list.setValue(false, "isDismissed");
-                    list.setValue(false, "isRead");
-                  }
-                }
-              });
+              setNodeLocalDefaultValues(edge.node.id);
               return (
                 <Listing key={`${i}:${edge.node.id}`} listing={edge.node} />
               );
