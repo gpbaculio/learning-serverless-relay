f7706b58080b796e27112b42fdfa048d483c9ebb
gpbaculio <gpbaculio@gmail.com>
2021-09-01 11:23:09 +0800

    add app + drawer test

---
+++ reddit-top/src/Drawer/test.constants.ts
@@ -0,0 +1,115 @@
+export const MockDrawerViewer = () => ({
+  user: "guest",
+  id: "Vmlld2VyOmd1ZXN0LWlk",
+  listings: {
+    pageInfo: {
+      startCursor: "YXJyYXljb25uZWN0aW9uOjA=",
+      endCursor: "YXJyYXljb25uZWN0aW9uOjY=",
+      hasNextPage: true,
+      hasPreviousPage: false,
+    },
+    edges: [
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjA=",
+        node: {
+          id: "TGlzdDp0M19wZmE1ajg=",
+          title:
+            "He's been trying to get a PS5 for months - I just happened to find one my local Curry's. He's gonna freak when he wakes up",
+          created: 1630429696,
+          num_comments: 2724,
+          thumbnail:
+            "https://b.thumbs.redditmedia.com/Y1309G6Yv077MAGn8mvvCD7_Iq62YhyxjGAVwgRVofA.jpg",
+          author: "Yevrah95",
+          name: "t3_pfa5j8",
+          __typename: "List",
+        },
+      },
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjE=",
+        node: {
+          id: "TGlzdDp0M19wZjlqaXQ=",
+          title:
+            "Man gets electrocuted while holding child. Red shirt guy saves the day",
+          created: 1630427873,
+          num_comments: 4178,
+          thumbnail:
+            "https://a.thumbs.redditmedia.com/szXFQxAARqHwL5wpd9mI0drMa3iKQp4vIi-Nsh3RZn0.jpg",
+          author: "regian24",
+          name: "t3_pf9jit",
+          __typename: "List",
+        },
+      },
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjI=",
+        node: {
+          id: "TGlzdDp0M19wZjY5c3A=",
+          title: "Thick, beautiful and gorgeous hair...",
+          created: 1630417886,
+          num_comments: 1469,
+          thumbnail:
+            "https://b.thumbs.redditmedia.com/Zf_KS0Wy1T0pE_3d4rBTYbGTEeJ_E0hrIMjdeORanLA.jpg",
+          author: "Sensitive_Attitude27",
+          name: "t3_pf69sp",
+          __typename: "List",
+        },
+      },
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjM=",
+        node: {
+          id: "TGlzdDp0M19wZmE3aTE=",
+          title: "Mens fantasies",
+          created: 1630429853,
+          num_comments: 2664,
+          thumbnail:
+            "https://b.thumbs.redditmedia.com/VTowzL2sD06AcDFptGeJFQgW1ecL7IGnE3stfQOomUA.jpg",
+          author: "Straight_Orchid2834",
+          name: "t3_pfa7i1",
+          __typename: "List",
+        },
+      },
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjQ=",
+        node: {
+          id: "TGlzdDp0M19wZjZtZnk=",
+          title:
+            "BBC reporter randomly asking if they remember the 1976 FA Cup match, ends up asking the Liverpool goalkeeper who played in it (wholesome)",
+          created: 1630419017,
+          num_comments: 948,
+          thumbnail:
+            "https://b.thumbs.redditmedia.com/gBAsQ121fsuqS3lL953fQSUiiVp7FlBc0u9xeIGt4IA.jpg",
+          author: "crasshumor",
+          name: "t3_pf6mfy",
+          __typename: "List",
+        },
+      },
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjU=",
+        node: {
+          id: "TGlzdDp0M19wZjNiNm4=",
+          title: "This guy lmao",
+          created: 1630406604,
+          num_comments: 1066,
+          thumbnail:
+            "https://b.thumbs.redditmedia.com/PatEMtGE9kRS0HkKPjtDO3tI-kOAK98oFffTASoUhQg.jpg",
+          author: "Arl107",
+          name: "t3_pf3b6n",
+          __typename: "List",
+        },
+      },
+      {
+        cursor: "YXJyYXljb25uZWN0aW9uOjY=",
+        node: {
+          id: "TGlzdDp0M19wZmM2M3Q=",
+          title: "Guy saves woman (and her baby) from committing suicide",
+          created: 1630435717,
+          num_comments: 609,
+          thumbnail:
+            "https://b.thumbs.redditmedia.com/K9xWLgUpG65NWonykZClWBRQAfeHQR1xEMFXNKtbWOI.jpg",
+          author: "Known_Marzipan_2629",
+          name: "t3_pfc63t",
+          __typename: "List",
+        },
+      },
+    ],
+  },
+});
--- reddit-top/src/__tests__/App.test.tsx
+++ reddit-top/src/__tests__/App.test.tsx
@@ -1,10 +1,10 @@
 import { render } from "@testing-library/react";
 import { createMockEnvironment } from "relay-test-utils";
 
-import { RelayProvider } from "./components";
-import App from "./App";
+import { RelayProvider } from "../components";
+import App from "../App";
 
-test("renders App", () => {
+test("should render App", () => {
   const environment = createMockEnvironment();
 
   const screen = render(
