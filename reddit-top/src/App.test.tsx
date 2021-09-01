37e663015ce4fefcaa53a85e6cea73de7a9f52a5
gpbaculio <gpbaculio@gmail.com>
2021-09-01 11:01:30 +0800

    update app test

--- reddit-top/src/App.test.tsx
+++ reddit-top/src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { render } from "@testing-library/react";
+import { createMockEnvironment } from "relay-test-utils";
+
+import { RelayProvider } from "./components";
 import App from "./App";
 
-test("renders learn react link", () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("renders App", () => {
+  const environment = createMockEnvironment();
+
+  const screen = render(
+    <RelayProvider environment={environment}>
+      <App />
+    </RelayProvider>
+  );
+
+  expect(screen).toBeTruthy();
 });
